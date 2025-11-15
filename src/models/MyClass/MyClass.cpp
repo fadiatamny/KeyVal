3041afc9d0062eda24c64376fab10bb3354deb4f
Fadi Atamny <fadiatamny@gmail.com>
2025-11-16 00:13:43 +0200

    removed redundatn class

--- src/models/MyClass/MyClass.cpp
+++
@@ -1,6 +0,0 @@
-#include "Models/MyClass/MyClass.hpp"
-#include <iostream>
-
-MyClass::MyClass(const std::string& msg) : message(msg) {}
-void MyClass::print() const { std::cout << message << std::endl; }
-std::string MyClass::get_message() const { return message; }
