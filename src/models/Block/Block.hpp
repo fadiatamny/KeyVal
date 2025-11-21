f6c074e4801be5041c70749ddaac08e3ad1d1fc4
Fadi Atamny <fadiatamny@gmail.com>
2025-11-22 01:08:50 +0200

    addessing warnings and linting

--- src/models/Block/Block.hpp
+++ src/models/Block/Block.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include "../../types/Constants.hpp"
+#include <cstring>
 
-struct Block {
+class Block {
+public:
   char data[BLOCK_SIZE];
   BlockId block_id;
   int referenceCount;
   bool isDirty;
 
-  Block() = default;
+  Block() : block_id(0), referenceCount(0), isDirty(false) {
+    std::memset(data, 0, BLOCK_SIZE);
+  }
+
   Block(const Block &) = delete;
   Block &operator=(const Block &) = delete;
   Block(Block &&) = delete;
